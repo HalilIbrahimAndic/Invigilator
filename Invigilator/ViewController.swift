eecf3c1e7dc8effbc859d2632812282ed7e6ff1a
Halil Ibrahim Andic <hiandic@gmail.com>
2022-12-27 06:34:44 +0300

    Initial Commit

---
+++ Invigilator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Invigilator
+//
+//  Created by Halil Ibrahim Andic on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
